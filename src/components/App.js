25c0b7403e30bdfae2fba88270ee8d6cb145aafe
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2021-01-27 11:30:42 +0700

    Router, Form validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import ProductListContainer from "../containers/ProductListContainer";
 import ProductDetailContainer from "../containers/ProductDetailContainer";
+import ProductFormContainer from "../containers/ProductFormContainer";
 
 function App() {
   return (
     <div className="App">
-      <ProductDetailContainer />
-      <hr />
-      <ProductListContainer />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/form">Form</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <ProductDetailContainer />
+              <hr />
+              <ProductListContainer />
+            </div>
+          </Route>
+          <Route path="/form">
+            <ProductFormContainer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
